feafc2c37920ddc11b6e51ccb287da42954cf64e
isaabir98 <60342418+isaabir98@users.noreply.github.com>
2021-08-31 20:18:50 +0530

    Add files via upload

---
+++ Customer_Login/Customer_Login/Models/DbAuthentication.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer_Login.Models
+{
+    public class DbAuthentication : IdentityDbContext<ApplicationUser>
+    {
+
+
+        public DbAuthentication(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser>ApplicationUsers{get; set;}
+    
+    
+    }
+       
+
+       
+}
